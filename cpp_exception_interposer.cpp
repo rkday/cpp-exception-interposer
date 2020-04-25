4bc5d1619d1867b366d1f9cf038a1df3a69ce933
Rob Day <rkd@rkd.me.uk>
2020-04-25 23:32:01 +0100

    Clarifying typedef

--- cpp_exception_interposer.cpp
+++ cpp_exception_interposer.cpp
@@ -3,6 +3,9 @@
 #include <backtrace.h>
 #include <typeinfo>
 
+// Function signature of cxa_throw - returns nothing, accepts three arguments (pointer to void, pointer to type info, function pointer)
+typedef void (*cxa_throw_fn_t)(void*, std::type_info*, void(void*));
+
 extern "C" void __cxa_throw(void* thrown_exception, std::type_info * tinfo, void (*dest)(void*))
 {
   struct backtrace_state * state = backtrace_create_state (NULL, 0, NULL, NULL);
@@ -11,8 +14,9 @@ extern "C" void __cxa_throw(void* thrown_exception, std::type_info * tinfo, void
   backtrace_print(state, 0, stderr);
   fprintf(stderr, "*** Proceeding with C++ exception handling ***\n");
 
+  // Get a pointer to the real __cxa_throw function
+  cxa_throw_fn_t real_function = reinterpret_cast<cxa_throw_fn_t>(dlsym(RTLD_NEXT, "__cxa_throw"));
   // Call the real __cxa_throw
-  void* (*real_function)(void*, void*, void(void*)) = reinterpret_cast<void *(*)(void *, void *, void (*)(void *))>(dlsym(RTLD_NEXT, "__cxa_throw"));
   real_function(thrown_exception, tinfo, dest);
 }
 
